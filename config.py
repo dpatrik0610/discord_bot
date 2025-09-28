2f616eb448c10bd5a7195eff6139c89bc69ad2e7
Dobos Patrik <d.patrik0610@gmail.com>
2025-09-28 17:09:23 +0200

    railway deployment

--- config.py
+++ config.py
@@ -1,9 +1,10 @@
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 API_BASE_URL = os.getenv("API_BASE_URL", "http://localhost:8000")
 COMMAND_PREFIX = os.getenv("COMMAND_PREFIX", "/")
-TEST_GUILD_ID= int(os.getenv("TEST_GUILD_ID", "0"))
+TEST_GUILD_ID = int(os.getenv("TEST_GUILD_ID", "0")) if os.getenv("TEST_GUILD_ID") else None
+
+# Validate required variables
+if not DISCORD_TOKEN:
+    raise ValueError("DISCORD_TOKEN environment variable is required")
